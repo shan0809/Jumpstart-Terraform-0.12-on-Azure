11082d5df39b543e4143942cf586dd4888172bc9
shantanu das <shan@shantanus-MacBook-Air.local>
2020-02-22 19:52:22 +0530

    backend state

---
+++ lecture3/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "AzureRMResource"
+    storage_account_name = "azurermtfdemostorage"
+    container_name       = "tfdemocontainer0"
+    key                  = "prod.terraform.tfstate"
+  }
+}
